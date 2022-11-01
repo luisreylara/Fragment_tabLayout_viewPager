1f8481b556d9f5344118cb03a9708e63b17c05f2
Luis Rey Lara <luisreylara@hotmail.com>
2022-11-01 10:06:55 -0600

    Creado el AdaptadorFragment

--- app/src/main/java/com/example/fragment_tablayout_viewpager2/MainActivity.java
+++ app/src/main/java/com/example/fragment_tablayout_viewpager2/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.fragment_tablayout_viewpager2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout=findViewById(R.id.tabLayout1);
+        viewPager2=findViewById(R.id.viewPager2);
+
+        viewPager2.setAdapter(new AdaptadorFragment(
+                getSupportFragmentManager(),getLifecycle()
+        ));
+    }
+
+    private class AdaptadorFragment extends FragmentStateAdapter {
+        public AdaptadorFragment(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return null;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
